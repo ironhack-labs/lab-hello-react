f2d936476cd313b49c84a5024506518ee3e6f97a
Osvaldo Picazo <odpicazo@gmail.com>
2021-09-20 16:00:16 +0200

    moved images folder to src and imported the images

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,4 +1,10 @@
-import logo from './logo.svg';
+import logo from './images/ironhack-logo-xs.png';
+import menuButton from './images/menu-top-xs.png';
+import declarative from './images/icon1.png';
+import components from './images/icon2.png';
+import singleWay from './images/icon3.png';
+import jsx from './images/icon4.png';
+
 import './App.css';
 
 function App() {
@@ -8,8 +14,8 @@ function App() {
       <div className="App-top-page">
   
         <div className="Nav-bar">
-          <img src='./images/ironhack-logo-xs.png' />
-          <img src='./images/menu-top-xs.png' />
+          <img src={logo} />
+          <img src={menuButton} />
         </div>
 
         <div className="App-Intro-Box">
@@ -23,25 +29,25 @@ function App() {
       <div className="App-bottom-page">
 
         <div className="App-bottom-container">
-          <img src='./images/icon1.png' />
+          <img src={declarative} />
           <h2>Declarative</h2>
           <h4>React makes it painless to create interactive UIs.</h4>
         </div>
 
         <div className="App-bottom-container">
-          <img src='./images/icon2.png' />
+          <img src={components} />
           <h2>Components</h2>
           <h4>Build encapsulated components that manage their state.</h4>
         </div>
 
         <div className="App-bottom-container">
-          <img src='./images/icon3.png' />
+          <img src={singleWay} />
           <h2>Single-Way</h2>
           <h4>A set of immutable values are passed to the component's.</h4>
         </div>
 
         <div className="App-bottom-container">
-          <img src='./images/icon4.png' />
+          <img src={jsx} />
           <h2>JSX</h2>
           <h4>Statically-typed designed to run on modern browsers.</h4>
         </div>
