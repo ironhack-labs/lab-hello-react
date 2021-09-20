de3f4a8c8e9ec046902602ab8b4e06218bee4998
Catarina Resende <41744553+anaresende@users.noreply.github.com>
2021-09-20 13:26:31 +0100

    really done

--- hello-react/src/components/card/Card.jsx
+++ hello-react/src/components/card/Card.jsx
@@ -3,6 +3,7 @@ function Card (props) {
     return (
         <div className="declarativeCard">
           <img src={props.image} alt="declarative icon"/>
+          <h3>{props.title}</h3>      
           <p>{props.information}</p>
         </div>
 
