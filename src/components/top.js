2811638ccbdab7b4472215a4f89855914222bf85
Yoel Martin <yoelmartinfornieles@gmail.com>
2021-09-20 17:05:22 +0200

    Features: lab done, some styling pending

---
+++ src/components/top.js
@@ -0,0 +1,31 @@
+function Top () {
+	return (
+		<>
+			<div className="top">
+				<div className = "navbar">
+					<div>
+						<img src="images/ironhack-logo-xs.png" alt="IH logo"/>
+					</div>
+					<div>
+						<img src="images/menu-top-xs.png" alt="burguer"/>
+					</div>
+				</div>
+				<div class="main">
+					<div class="main-title">
+						<h1>Say Hello to <br/> ReactJS</h1>
+					</div>
+					<div class="description">
+						<p>You will learn how to use <br/> 
+						the most popular frontend library, <br/> 
+						and become a super Ninja developer</p>
+					</div>
+				</div>
+				<div class="btn">
+					<button>Awesome!</button>
+				</div>
+			</div>
+		</>
+	)
+}
+
+export default Top;
