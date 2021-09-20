bb1c45d52b67ba34577c99127cf1fa2a576bc0e8
Ana Martins <ana.af.martins@gmail.com>
2021-09-20 12:51:32 +0200

    lab done

---
+++ hello-react-app/src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import Nav from "./components/Nav";
+import Header from "./components/Header";
+import Features from "./components/Features";
+
+function App() {
+  return (
+    <div className="App">
+      <Nav></Nav>
+      <Header></Header>
+      <Features></Features>
+    </div>
+  );
+}
+
+export default App;
