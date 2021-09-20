bbf2072b1023a61725cc57a2cf13e0958b151c6d
Espe Cabrer <mecabrerv@gmail.com>
2021-09-20 18:35:29 +0200

    Done

---
+++ lab-hello-react/src/components/first-part.js
@@ -0,0 +1,13 @@
+
+
+function firstPart() {
+    return (
+        <div className="first-part">
+            <h1>Say hello to ReactJS</h1>
+            <p>You will learn how to use <br/> the most popular frontend library, <br/> and become a super Ninja developer.</p>
+            <button>Awesome!</button>
+        </div>
+    );
+}
+
+export default firstPart;
