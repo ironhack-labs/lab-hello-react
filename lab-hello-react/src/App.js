0bac06dbfd362f9b3fdcb4647fe912cbf415a97f
Jaume Ballester <86907672+jaumebt90@users.noreply.github.com>
2021-09-20 17:33:17 +0200

    some css

--- lab-hello-react/src/App.js
+++ lab-hello-react/src/App.js
@@ -32,20 +32,29 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <div>
-          <img src={logo} className="App-logo" alt="logo" />
-          <img src={menu} className="App-menu" alt="logo" />
+        <nav className="navbar">
+          <div className="logo">
+            <img src={logo} alt="logo" />
+          </div>
+          <div className="menu">
+            <img src={menu} alt="logo" />
+          </div>
+        </nav>
+        <div className="text">
+          <div>
+            <h1>
+              Say hello to <br /> ReactJS
+            </h1>
+          </div>
+          <div>
+            <h4>
+              You will learn how to use <br /> the most popular frontend
+              library,
+              <br /> and become a super Ninja developer.
+            </h4>
+          </div>
+          <button> Awesome!</button>
         </div>
-        <div>
-          <h1>
-            Say hello to <br /> ReactJS
-          </h1>
-          <h4>
-            You will learn how to use <br /> the most popular frontend library,
-            <br /> and become a super Ninja developer.
-          </h4>
-        </div>
-        <button> Awesome!</button>
       </header>
       <body className="cards">
         <Card card={card1} />
