d796603cfbac068e9fc4b44768c757b8db9f995e
Telma Rosário <telmarrosario@gmail.com>
2021-11-29 15:41:30 +0000

    done

---
+++ src/Navbar.js
@@ -0,0 +1,20 @@
+
+
+function Navbar () {
+
+    return (
+        
+    <div className= "Navbar">
+        <img src="https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/labs/landing-page/ironhack-logo-xs.png
+            " alt="ironhack logo"/> 
+        <img src="https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/labs/landing-page/menu-top-xs.png
+        " alt="menu" /> 
+    </div>
+
+    
+    
+    )
+      
+}
+
+export default Navbar
