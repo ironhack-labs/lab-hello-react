430774f74b7ecec11a7189ff040b1afbbf1a06f4
David <ddaarr@live.com.pt>
2021-11-29 15:26:50 +0000

    Finished lab

---
+++ src/components/Headline.js
@@ -0,0 +1,19 @@
+function Headline() {
+  return (
+    <div className="Headline">
+      <h2>
+        Say hello to <br /> ReactJS
+      </h2>
+      <p>
+        You will learn how to use
+        <br />
+        the most popular frontend library,
+        <br />
+        and become a super Ninja developer.
+      </p>
+      <button>Awesome!</button>
+    </div>
+  );
+}
+
+export default Headline;
