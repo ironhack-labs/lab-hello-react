e6335d55d11dbac7a29655d9a5e7aa56e0d4ef64
Anne Dantas <annedantas@gmail.com>
2021-11-29 15:25:12 +0000

    back to multiple components

--- src/hello.js
+++ src/hello.js
@@ -8,45 +8,18 @@ import icon4 from "./images/icon4.png";
 
 const Hello = () => {
   return (
-    <div>
-      <div className="hello-txt">
-        <nav>
-          <img src={logo} alt="" />
-          <img src={menu} alt="" />
-        </nav>
-        <div className="txt-and-btn">
-          <h1>Say hello to ReactJS</h1>
-          <p>
-            You will learn how to use the most popular frontend library, and
-            become a super Ninja Developer.
-          </p>
-          <button> Awesome! </button>
-        </div>
-      </div>
-      <div className="second-part">
-        <div className="card">
-          <img src={icon1} alt="" />
-          <h3>Declarative</h3>
-          <p>React makes it painless to create interective UIs.</p>
-        </div>
-
-        <div className="card">
-          <img src={icon2} alt="" />
-          <h3>Components</h3>
-          <p>Build encapsulated components that manage their state.</p>
-        </div>
-
-        <div className="card">
-          <img src={icon3} alt="" />
-          <h3>Single-Way</h3>
-          <p>A set of immutable values are passed to the component's.</p>
-        </div>
-
-        <div className="card">
-          <img src={icon4} alt="" />
-          <h3>JSX</h3>
-          <p>Statucally-typed, designed to run on modern browsers.</p>
-        </div>
+    <div className="hello-txt">
+      <nav>
+        <img src={logo} alt="" />
+        <img src={menu} alt="" />
+      </nav>
+      <div className="txt-and-btn">
+        <h1>Say hello to ReactJS</h1>
+        <p>
+          You will learn how to use the most popular frontend library, and
+          become a super Ninja Developer.
+        </p>
+        <button> Awesome! </button>
       </div>
     </div>
   );
