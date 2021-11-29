10d0a7ef8ca90625d4d63cd9f0eb1a8ff2e3b95f
nachbr91 <nabero91@gmail.com>
2021-11-29 17:57:34 +0100

    Lab done

---
+++ lab-hello-react/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import LandingPage from './LandingPage';
+
+const App = () => {
+  return (
+    <div className="App">
+      <LandingPage />
+    </div>
+  )
+};
+
+export default App;
