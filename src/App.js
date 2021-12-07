f593a3ebf941568bff7ae09293c467dcdfb474d5
KuraiTsukino <sandym3000@hotmail.com>
2021-12-06 22:12:40 -0600

    Complete done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-
+// 1. Importaciones
 import './App.css';
+import Body from './components/Body';
+import Header from './components/Header';
+
+
+// 2. Función
 
 function App() {
   return (
     <div className="App">
-      
+      <Header />
+
+      <Body />
     </div>
   );
 }
 
+// 3. Exportación
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,27 @@
+// 1. Importaciones
+import React from 'react'
+import logo from "./../images/ironhack-logo-xs.png";
+import menu from "./../images/menu-top-xs.png"
+
+// 2. Función
+
+export default function Header() {
+    return (
+        <>
+            <header class="header">
+                <section>
+                    <img class="logo" src={logo} alt="logo"/>
+                    <img class="menu" src={menu} alt="menu" />
+                </section>
+
+                <section class="title">
+                    <h1>Say hello to ReactJS</h1>
+                    <h5>You will learn how to use the most popular frontend library, and become a super Ninja developer.</h5>
+                    <button class="button"> Awesome!</button>
+                </section>
+
+
+            </header>
+        </>
+    )
+}
