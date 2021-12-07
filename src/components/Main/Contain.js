5265906e43666cd4d137c7159741d9b8621896a7
Edgar Uco <edgarin@gmail.com>
2021-12-06 20:52:58 -0600

    Lab ih-react-01 done

---
+++ src/components/Main/Contain.js
@@ -0,0 +1,79 @@
+import React from 'react'
+
+export default function Contain() {
+      return (
+          <div>
+              <div class="blogs__wraper bg-white  py-20 px-20">
+                  <div class="flex justify-between items-center">
+                      <div class="blogs bg-white mr-5">
+                          <img
+                              src="https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/labs/landing-page/icon1.png"
+                              class=""
+                              alt="img1"
+                          />
+                          <div class="p-2">
+                              <h1 class="text-2xl font-bold text-gray-800 py-2">
+                                  Declarative
+                              </h1>
+                              <p class="bg-white text-sm text-black">
+                                  React makes it painless to create interactive
+                                  UIs.
+                              </p>
+                          </div>
+                      </div>
+
+                      <div class="blogs bg-white mr-5">
+                          <img
+                              src="https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/labs/landing-page/icon2.png"
+                              class=""
+                              alt="img2"
+                          />
+                          <div class="p-2">
+                              <h1 class="text-2xl font-bold text-gray-800 py-2">
+                                  Components
+                              </h1>
+                              <p class="bg-white text-sm text-black">
+                                  Build encapsulated components that manage
+                                  their state.
+                              </p>
+                          </div>
+                      </div>
+
+                      <div class="blogs bg-white mr-5">
+                          <img
+                              src="https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/labs/landing-page/icon3.png"
+                              class=""
+                              alt="img3"
+                          />
+                          <div class="p-2">
+                              <h1 class="text-2xl font-bold text-gray-800 py-2">
+                                  Single-Way
+                              </h1>
+                              <p class="bg-white text-sm text-black">
+                                  A set of immutable values are passed to the
+                                  component's.
+                              </p>
+                          </div>
+                      </div>
+
+                      <div class="blogs bg-white mr-5">
+                          <img
+                              src="https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/labs/landing-page/icon4.png"
+                              class=""
+                              alt="img4"
+                          />
+                          <div class="p-2">
+                              <h1 class="text-2xl font-bold text-gray-800 py-2">
+                                  JSX
+                              </h1>
+                              <p class="bg-white text-sm text-black">
+                                  Statically-typed, designed to run on modern
+                                  browsers.
+                              </p>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+          </div>
+      );
+}
