e52ba8b8d13a079a7f507c105431bf301442c3ae
Amanda Rodrigues <amanda.rodrigues@lego.com>
2022-02-21 12:54:14 +0100

    done

---
+++ src/components/Section2.js
@@ -0,0 +1,49 @@
+// React Component <Section2 />
+
+function Section2() {
+  return (
+    <div id="sec2">
+      <div id="items">
+        <img
+          src="https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/labs/landing-page/icon1.png"
+          alt="logo"
+        />
+        <h4>Declarative</h4>
+        <p>React makes it painless to create interactive UIs.</p>
+        <button id="btn">Awesome!</button>
+      </div>
+
+      <div id="items">
+        <img
+          src="https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/labs/landing-page/icon2.png"
+          alt="logo"
+        />
+        <h4>Declarative</h4>
+        <p>React makes it painless to create interactive UIs.</p>
+        <button id="btn">Awesome!</button>
+      </div>
+
+      <div id="items">
+        <img
+          src="https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/labs/landing-page/icon3.png"
+          alt="logo"
+        />
+        <h4>Declarative</h4>
+        <p>React makes it painless to create interactive UIs.</p>
+        <button id="btn">Awesome!</button>
+      </div>
+
+      <div id="items">
+        <img
+          src="https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/labs/landing-page/icon4.png"
+          alt="logo"
+        />
+        <h4>Declarative</h4>
+        <p>React makes it painless to create interactive UIs.</p>
+        <button id="btn">Awesome!</button>
+      </div>
+    </div>
+  );
+}
+
+export default Section2;
