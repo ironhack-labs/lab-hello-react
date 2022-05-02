0d456cd13ff92dc71a074526517103764dea5e7d
Julian Rivera <julianarevircoins@gmail.com>
2022-05-02 16:49:11 -0400

    css/different component structure

--- lab-hello-react/src/App.jsx
+++ lab-hello-react/src/App.jsx
@@ -1,18 +1,12 @@
 import './App.css';
 import Header from './components/Header';
-import ImageOne from './components/ImageOne';
-import ImageTwo from './components/ImageTwo';
-import ImageThree from './components/ImageThree';
-import ImageFour from './components/ImageFour';
+import Section from './components/Section';
 
 function App() {
   return (
     <div className='App'>
       <Header />
-      <ImageOne />
-      <ImageTwo />
-      <ImageThree />
-      <ImageFour />
+      <Section />
     </div>
   );
 }
--- lab-hello-react/src/components/Section.jsx
+++ lab-hello-react/src/components/Section.jsx
@@ -1,3 +1,17 @@
-function Section() {
-  return <div></div>;
-}
+import ImageOne from '../components/ImageOne';
+import ImageTwo from '../components/ImageTwo';
+import ImageThree from '../components/ImageThree';
+import ImageFour from '../components/ImageFour';
+
+const Section = () => {
+  return (
+    <div className='section'>
+      <ImageOne />
+      <ImageTwo />
+      <ImageThree />
+      <ImageFour />
+    </div>
+  );
+};
+
+export default Section;
