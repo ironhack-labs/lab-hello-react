12bca0ad7e43d6698d0e6a734605928ce5927a65
Sofiane M <poubellepropre8@gmail.com>
2022-05-23 17:20:19 +0200

    lab finished

--- hello-react/src/App.jsx
+++ hello-react/src/App.jsx
@@ -11,54 +11,47 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <div>
 
-    <div>
-
-      <div className="Navbar">
-      <img src={ironLogo} alt="ironLogo" width='50' />
-      <img src={rightThing} alt="rightThing" width='50' />
+        <div className="description">
+          <div className="nav">
+          <img src={ironLogo} alt="ironLogo" width="50" />
+          <img src={rightThing} alt="rightThing" width="50" />
+          </div>
+          <h1>Say hello to <br /> ReactJs</h1>
+          <p>
+            You will learn how to use <br /> the most popular frontend library, <br /> and
+            become a super Ninja developer
+          </p>
+          <button>Awesome!</button>
+        </div>
       </div>
 
-      <h1>Say hello to ReactJs</h1>
-      <p>You will learn how to use <br></br>
-      the most popular frontend library <br></br>
-      and become a super Ninja developer
-      </p>
-      <button>Awesome!</button>
-    </div>
+      <div className="Second-panel">
+        <div className="element">
+          <img src={toolLogo} alt="toolLogo" />
+          <h3>Declarative</h3>
+          <p>React makes it painless to create interactive UIs</p>
+        </div>
 
-    <div className="Second-panel">
-    
-    <div className="element">
-      <img src={toolLogo} alt="toolLogo" />
-      <h3>Declarative</h3>
-      <p>React makes it painless to create interactive UIs
-      </p>
-    </div>
+        <div className="element">
+          <img src={pcLogo} alt="pcLogo" />
+          <h3>Components</h3>
+          <p>React makes it painless to create interactive UIs</p>
+        </div>
 
-    <div className="element">
-      <img src={pcLogo} alt="pcLogo" />
-      <h3>Components</h3>
-      <p>React makes it painless to create interactive UIs
-      </p>
-    </div>
+        <div className="element">
+          <img src={screenLogo} alt="screenLogo" />
+          <h3>Single-Way</h3>
+          <p>React makes it painless to create interactive UIs</p>
+        </div>
 
-    <div className="element">
-      <img src={screenLogo} alt="screenLogo" />
-      <h3>Single-Way</h3>
-      <p>React makes it painless to create interactive UIs
-      </p>
-    </div>
-
-    <div className="element">
-      <img src={codeLogo} alt="codeLogo" />
-      <h3>JSX</h3>
-      <p>React makes it painless to create interactive UIs
-      </p>
-    </div>
-
-    </div>
-
+        <div className="element">
+          <img src={codeLogo} alt="codeLogo" />
+          <h3>JSX</h3>
+          <p>React makes it painless to create interactive UIs</p>
+        </div>
+      </div>
     </div>
   );
 }
@@ -89,4 +82,3 @@ export default App;
 // }
 
 // export default App;
-
