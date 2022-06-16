4b6ec07405425d7dff30675bd9cc2140815b28cf
EugeBr <eugebordenave@gmail.com>
2022-06-16 13:52:22 +0200

    Lab done

--- new-react-app/src/App.js
+++ new-react-app/src/App.js
@@ -1,8 +1,13 @@
 import './App.css';
+import DarkDiv from './DarkDiv';
+import Icons from './Icons';
 
 function App() {
   return (
-    <div className="App"></div>
+    <div className="App">
+      <DarkDiv />
+      <Icons />
+    </div>
   );
 }
 
