0caba81c68772016700de29352f4b4cbb91d8180
Lukas Baur <baurlukas89@gmail.com>
2022-06-17 10:12:56 +0200

    done

---
+++ my-first-react-app/src/App.js
@@ -0,0 +1,14 @@
+// src/App.js
+import "./App.css";
+import Header from "./Header";
+import Features from "./Features";
+
+function App() {
+  return (
+    <div className="App">
+      <Header></Header>
+      <Features></Features>
+    </div>
+  );
+}
+export default App;
