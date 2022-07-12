5230fc9a34976e3de7ff09a3364a42cb88ec7d77
AitorSantaeugenia <aitor.j.s.m@gmail.com>
2022-07-12 19:59:24 +0200

    updated

--- lab-hello-react/src/App.js
+++ lab-hello-react/src/App.js
@@ -1,72 +1,57 @@
-// import logo from './logo.svg';
-// import './App.css';
-
-// function App() {
-// 	return (
-// 		<div className="App">
-// 			<header className="App-header">
-// 				<img src={logo} className="App-logo" alt="logo" />
-// 				<p>
-// 					Edit <code>src/App.js</code> and save to reload.
-// 				</p>
-// 				<a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-// 					Learn React
-// 				</a>
-// 			</header>
-// 		</div>
-// 	);
-// }
+/*
+En comentario si lo hicistéis sin componentes
+*/
 
 // export default App;
 //---------------------------------------------------
 // src/App.js
-import './App.css';
+import "./App.css";
 //load images
-import logo from './images/logo.png';
-import menu from './images/menu-top-xs.png';
-import image1 from './images/icon1.png';
-import image2 from './images/icon2.png';
-import image3 from './images/icon3.png';
-import image4 from './images/icon4.png';
+import logo from "./images/logo.png";
+import menu from "./images/menu-top-xs.png";
+import image1 from "./images/icon1.png";
+import image2 from "./images/icon2.png";
+import image3 from "./images/icon3.png";
+import image4 from "./images/icon4.png";
 
 //doing components
-import LogoImage from './components/LogoImage';
-import MenuImage from './components/MenuImage';
-import HeaderText from './components/HeaderText';
-import HeaderButton from './components/HeaderButton';
-import MenuIcons from './components/MenuIcons';
+import LogoImage from "./components/LogoImage";
+import MenuImage from "./components/MenuImage";
+import HeaderText from "./components/HeaderText";
+import HeaderButton from "./components/HeaderButton";
+import MenuIcons from "./components/MenuIcons";
 
 function App() {
-	return (
-		<div className="container">
-			<header className="header">
-				<div className="headerImages">
-					<LogoImage image={logo} />
-					{/* <img src={logo} alt="logo" /> */}
-					<MenuImage image={menu} />
-					{/* <a href="/">
+  return (
+    <div className="container">
+      <header className="header">
+        <div className="headerImages">
+          <LogoImage image={logo} />
+          {/* <img src={logo} alt="logo" /> */}
+          <MenuImage image={menu} />
+          {/* <a href="/">
 						<img src={menu} alt="menu" id="imageMenu" />
 					</a> */}
-				</div>
-				<div className="headerContent">
-					{/* <h1 className="headerContentH1">Say hello to ReactJS</h1>
+        </div>
+        <div className="headerContent">
+          {/* <h1 className="headerContentH1">Say hello to ReactJS</h1>
 					<p className="headerContentP">
 						You will learn how to use<br /> the most popular frontend library,<br /> and become a super
 						Ninja developer.
 					</p> */}
-					<HeaderText />
-					<HeaderButton />
-					{/* <a className="headerContentButton" href="/">
+          <HeaderText />
+          <HeaderButton />
+          {/* <a className="headerContentButton" href="/">
 						Awesome!
 					</a> */}
-				</div>
-			</header>
-			<section className="section">
-				<MenuIcons image={image1} />
-				<MenuIcons image={image2} />
-				<MenuIcons image={image3} />
-				<MenuIcons image={image4} />
-				{/* <div>
+        </div>
+      </header>
+      <section className="section">
+        <MenuIcons image={image1} />
+        <MenuIcons image={image2} />
+        <MenuIcons image={image3} />
+        <MenuIcons image={image4} />
+        {/* <div>
 					<img src={image1} alt="logo" />
 					<h2 className="sectionH1"> Declarative</h2>
 					<p className="sectionP">
@@ -94,8 +79,8 @@ function App() {
 						Statically-typed,<br /> designed to run on<br /> modern browsers.
 					</p>
 				</div> */}
-			</section>
-		</div>
-	);
+      </section>
+    </div>
+  );
 }
 export default App;
