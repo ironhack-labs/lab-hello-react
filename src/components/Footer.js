b9cb2bc96204d31a2e6642f05c9c5afbf941ae3b
CheloCr <85573546+CheloCr@users.noreply.github.com>
2022-07-22 22:21:33 -0500

    lab done

---
+++ src/components/Footer.js
@@ -0,0 +1,81 @@
+import React from 'react'
+
+export default function Footer() {
+
+
+
+  return (
+    <>
+    <div class="lg:flex              lg:justify-center bg-gray-900">
+  <div class="pt-12 px-4 sm:px-6 lg:px-8 lg:pt-20">
+    <div class="text-center">
+
+      
+     
+    </div>
+  </div>
+
+  <div class="mt-16 bg-white pb-12 lg:mt-20 lg:pb-20">
+    <div class="relative z-0">
+      <div class="absolute inset-0 h-5/4 bg-gray-900 lg:h-3/7"></div>
+      <div class=" max-w-7x2 mx-20 px-4 sm:px-6 lg:px-8">
+        <div class="relative lg:grid lg:grid-cols-7">
+          <div class="mx-auto max-w-md lg:mx-0 lg:max-w-none lg:col-start-1 lg:col-end-3 lg:row-start-2 lg:row-end-3">
+            <div class="h-full flex flex-col rounded-lg shadow-lg overflow-hidden lg:rounded-none lg:rounded-l-lg">
+              <div class="flex-1 flex flex-col">
+                <div class="bg-white px-6 py-10">
+                  <div>
+                    <img src="https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/labs/landing-page/icon1.png" alt="" />
+                  </div>
+                </div>
+                <div class="flex-1 flex flex-col justify-between border-t-2 border-gray-100 p-6 bg-gray-50 sm:p-10 lg:p-6 xl:p-10">
+                <h3 class="text-center text-3xl font-semibold text-gray-900 sm:-mx-6" id="tier-growth">Declarative</h3>
+                <span class="text-2xl font-medium text-gray-500"> React makes it painiess to create interactive UIs </span>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="mx-auto max-w-md lg:mx-0 lg:max-w-none lg:col-start-3 lg:col-end-5 lg:row-start-2 lg:row-end-3">
+            <div class="h-full flex flex-col rounded-lg shadow-lg overflow-hidden lg:rounded-none lg:rounded-l-lg">
+              <div class="flex-1 flex flex-col">
+                <div class="bg-white px-6 py-10">
+                  <div>
+                    <img src="https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/labs/landing-page/icon2.png" alt="" />
+                  </div>
+                </div>
+                <div class="flex-1 flex flex-col justify-between border-t-2 border-gray-100 p-6 bg-gray-50 sm:p-10 lg:p-6 xl:p-10">
+                <h3 class="text-center text-3xl font-semibold text-gray-900 sm:-mx-6" id="tier-growth">Components</h3>
+                <span class="text-2xl font-medium text-gray-500"> Build encapsualted components tjat manage their state.</span>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="mx-auto max-w-md lg:mx-0 lg:max-w-none lg:col-start-5 lg:col-end-7 lg:row-start-2 lg:row-end-3">
+            <div class="h-full flex flex-col rounded-lg shadow-lg overflow-hidden lg:rounded-none lg:rounded-l-lg">
+              <div class="flex-1 flex flex-col">
+                <div class="bg-white px-6 py-10">
+                  <div>
+                    <img src="https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/labs/landing-page/icon4.png" alt="" />
+                  </div>
+                </div>
+                <div class="flex-1 flex flex-col justify-between border-t-2 border-gray-100 p-6 bg-gray-50 sm:p-10 lg:p-6 xl:p-10">
+                <h3 class="text-center text-3xl font-semibold text-gray-900 sm:-mx-6" id="tier-growth">JSX</h3>
+                <span class="text-2xl font-medium text-gray-500"> Statically-typed. Designed to ru on modern browsers. </span>
+                </div>
+              </div>
+            </div>
+          </div>
+
+         
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+    
+    
+    </>
+  )
+}
