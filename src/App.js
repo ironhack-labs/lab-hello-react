93063c54a720df0c46a9e77cd0f12cda6fa235e8
Renata Inojosa <inojosa.renata@gmail.com>
2022-08-03 12:40:37 -0300

    doing iteration 4

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import './App.css';
+import ironhackLogo from './images/ironhack-logo-xs.png'
+const heading = <h1>Say hello to ReactJS!</h1>
 
 function App() {
   return (
-    <div className="App"></div>
+    <div className="App">{heading}
+    <img src={ironhackLogo} alt="ironhack logo" />
+    <p>You will learn how to use the most popular frontend library, and become a super Ninja developer.</p>
+    </div>
   );
 };
 
