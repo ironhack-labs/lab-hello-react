214fba3c01512ba3526d2509e0f3d196a9412a04
Renata Inojosa <inojosa.renata@gmail.com>
2022-08-06 08:01:18 -0300

    doing iterarion 4

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
-import './App.css';
-import ironhackLogo from './images/ironhack-logo-xs.png'
-const heading = <h1>Say hello to ReactJS!</h1>
+import "./App.css";
+import ironhackLogo from "./images/ironhack-logo-xs.png";
+import menutop from "./images/menu-top-xs.png";
+import iconOne from "./images/icon1.png";
+import iconTwo from "./images/icon2.png";
+import iconThree from "./images/icon3.png";
+import iconFour from "./images/icon4.png";
+import MyButton from "./components/MyButton";
+
+const heading = <h1>Say hello to ReactJS!</h1>;
+const introduction = (
+  <p>
+    You will learn how to use the most popular frontend library, and become a
+    super Ninja developer.
+  </p>
+);
 
 function App() {
   return (
-    <div className="App">{heading}
-    <img src={ironhackLogo} alt="ironhack logo" />
-    <p>You will learn how to use the most popular frontend library, and become a super Ninja developer.</p>
+    <div className="App-header">
+      <img src={ironhackLogo} alt="ironhack logo" />
+      <img src={menutop} alt="menu" />
+      {heading} {introduction}
+      <MyButton />
+      <h2>
+        <img src={iconOne} alt="icon one" />{" "}
+        <img src={iconTwo} alt="icon two" />
+        <img src={iconThree} alt="icon three" />
+        <img src={iconFour} alt="icon four" />
+      </h2>
     </div>
   );
-};
+}
 
 export default App;
---
+++ src/components/MyButton.js
@@ -0,0 +1,9 @@
+function MyButton() {
+    return (
+      <a href="https://ironhack.com">
+        <button className="btn">AWESOME!</button>
+      </a>
+    );
+  }
+
+  export default MyButton; 
