e1021c61f57c21485275b7c2ba6dca4267644c00
Renata Inojosa <inojosa.renata@gmail.com>
2022-08-06 18:19:47 -0300

    done

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import iconOne from "./images/icon1.png";
 import iconTwo from "./images/icon2.png";
 import iconThree from "./images/icon3.png";
 import iconFour from "./images/icon4.png";
-import MyButton from "./components/MyButton";
 
 const heading = <h1>Say hello to ReactJS!</h1>;
 const introduction = (
@@ -14,20 +13,52 @@ const introduction = (
     super Ninja developer.
   </p>
 );
+const textOne = <p>React makes it painless to create interactive UIs.</p>;
+const textTwo = <p>Build encapsulated components that mange their state.</p>;
+const textThree = (
+  <p>A set of immulable values are passed to the coponent's.</p>
+);
+const textFour = <p>Statically-type, designed to run on modern browsers.</p>;
 
 function App() {
   return (
-    <div className="App-header">
-      <img src={ironhackLogo} alt="ironhack logo" />
-      <img src={menutop} alt="menu" />
-      {heading} {introduction}
-      <MyButton />
-      <h2>
-        <img src={iconOne} alt="icon one" />{" "}
-        <img src={iconTwo} alt="icon two" />
-        <img src={iconThree} alt="icon three" />
-        <img src={iconFour} alt="icon four" />
-      </h2>
+    <div className="App">
+      <div className="nav">
+        <nav>
+          <img src={ironhackLogo} alt="ironhack logo" />
+          <img src={menutop} alt="menu" />
+        </nav>
+      </div>
+      <div className="App-header">
+        <div className="title">{heading}</div>
+
+        <div className="intro">{introduction}</div>
+
+        <button className="btn">AWESOME!</button>
+      </div>
+
+      <div className="cards">
+        <div className="cardOne">
+          <img src={iconOne} alt="icon-one" />
+          <h2>Declarative</h2>
+          <p>{textOne}</p>
+        </div>
+        <div className="cardTwo">
+          <img src={iconTwo} alt="icon-two" />
+          <h2>Components</h2>
+          <p>{textTwo}</p>
+        </div>
+        <div className="cardThree">
+          <img src={iconThree} alt="icon-three" />
+          <h2>Single-Way</h2>
+          <p>{textThree}</p>
+        </div>
+        <div className="cardFour">
+          <img src={iconFour} alt="icon-four" />
+          <h2>JSX</h2>
+          <p>{textFour}</p>
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/MyButton.js
+++
@@ -1,9 +0,0 @@
-function MyButton() {
-    return (
-      <a href="https://ironhack.com">
-        <button className="btn">AWESOME!</button>
-      </a>
-    );
-  }
-
-  export default MyButton; 
