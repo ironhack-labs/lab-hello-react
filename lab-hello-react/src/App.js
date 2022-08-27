3632a5fdb8e74cdfe57c346dd91c244dd57dbfc6
Adrian Perez <adrian.perez.conde22@gmail.com>
2022-08-27 11:21:24 +0200

    done x2

--- lab-hello-react/src/App.js
+++ lab-hello-react/src/App.js
@@ -11,44 +11,44 @@ function App() {
   return (
     <div className="App">
       <header className="Header">
-        <div class="d-flex justify-content-between">
-          <img src={ironhackLogo} alt="Ironhack Logo" class="p-4"/>
-          <img src={menu} alt="Menu" class="mx-2 p-5"/>
+        <div className="d-flex justify-content-between">
+          <img src={ironhackLogo} alt="Ironhack Logo" className="p-4"/>
+          <img src={menu} alt="Menu" className="mx-2 p-5"/>
         </div>
-        <h1 class="text-light fw-bold m-4 ps-3">
+        <h1 className="text-light fw-bold m-4 ps-3">
           Say hello to <br></br>
           ReactJS
         </h1>
 
-        <h5 class="text-light m-4 ps-3">
+        <h5 className="text-light m-4 ps-3">
           You will learn how to use <br></br>the most popular frontend library,<br></br>
           and become a super Ninja developer.
         </h5>
-        <div class="ms-4 ps-3 pb-5">
-          <button class="btn btn-light fw-bold btn-lg">Awesome!</button>
+        <div className="ms-4 ps-3 pb-5">
+          <button className="btn btn-light fw-bold btn-lg">Awesome!</button>
         </div>
       </header>
-      <div class="container pt-5">
-        <div class="row">
-        <div class="col">
+      <div className="container pt-5">
+        <div className="row">
+        <div className="col">
           <img src={icon1} alt="Declarative" className="Icon"/>
-          <h2 class="ms-3">Declarative</h2>
-          <p class="ms-3">React makes it <br></br>painless to create <br></br>interactive UIs.</p>
+          <h2 className="ms-3">Declarative</h2>
+          <p className="ms-3">React makes it <br></br>painless to create <br></br>interactive UIs.</p>
         </div>
-        <div class="col">
+        <div className="col">
           <img src={icon2} alt="Components" className="Icon"/>
-          <h2 class="ms-3">Components</h2>
-          <p class="ms-3">Build encapsulated <br></br>components that <br></br>manage their state.</p>
+          <h2 className="ms-3">Components</h2>
+          <p className="ms-3">Build encapsulated <br></br>components that <br></br>manage their state.</p>
         </div>
-        <div class="col">
+        <div className="col">
           <img src={icon3} alt="Single-Way" className="Icon"/>
-          <h2 class="ms-3">Single-Way</h2>
-          <p class="ms-3">A set of immutable <br></br>values are passed to <br></br>the component's.</p>
+          <h2 className="ms-3">Single-Way</h2>
+          <p className="ms-3">A set of immutable <br></br>values are passed to <br></br>the component's.</p>
         </div>
-        <div class="col">
+        <div className="col">
           <img src={icon4} alt="JSX" className="Icon"/>
-          <h2 class="ms-3">JSX</h2>
-          <p class="ms-3">Statically-typed, <br></br>designed to run on <br></br>modern browsers.</p>
+          <h2 className="ms-3">JSX</h2>
+          <p className="ms-3">Statically-typed, <br></br>designed to run on <br></br>modern browsers.</p>
         </div>
         </div>
         
