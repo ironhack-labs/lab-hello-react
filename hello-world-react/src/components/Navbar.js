e9da4a32e0abd2603af68d03de074e7748152344
Adriana Molina <amolinalopez05@gmail.com>
2022-08-27 12:18:48 +0200

    ite 1 2 et 3 done

---
+++ hello-world-react/src/components/Navbar.js
@@ -0,0 +1,15 @@
+
+function Navbar() {
+    return (
+      <nav>
+        <img
+          id="nav-img"
+          src="https://avatars.githubusercontent.com/u/54281267?s=280&v=4"
+          alt="logo"
+        />
+        <h1>Hello</h1>
+      </nav>
+    );
+  }
+  
+  export default Navbar;
