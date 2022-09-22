644a12ce2c26843e103401d0b4cf11faf6de1f26
Nuno Morais <nunodpmorais@gmail.com>
2022-09-22 18:39:25 +0100

    Done

---
+++ hello-react/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Features from "./components/Features";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <div className='App'>
+      <Header />
+      <Features />
+    </div>
+  );
+}
+
+export default App;
---
+++ hello-react/src/components/Features.js
@@ -0,0 +1,40 @@
+function Features() {
+  return (
+    <div className='features'>
+      <div className='feature'>
+        <img
+          src='https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/labs/landing-page/icon1.png'
+          alt='icon1'
+        />
+        <h3>Declarative</h3>
+        <p>React makes it painless to create interactive UIs.</p>
+      </div>
+      <div className='feature'>
+        <img
+          src='https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/labs/landing-page/icon2.png'
+          alt='icon2'
+        />
+        <h3>Components</h3>
+        <p>Build encapsulated components that manage their state.</p>
+      </div>
+      <div className='feature'>
+        <img
+          src='https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/labs/landing-page/icon3.png'
+          alt='icon3'
+        />
+        <h3>Single-Way</h3>
+        <p>A set of immutable values are passed to the component's.</p>
+      </div>
+      <div className='feature'>
+        <img
+          src='https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/labs/landing-page/icon4.png'
+          alt='icon4'
+        />
+        <h3>JSX</h3>
+        <p>Statically-typed, designed to run on modern browsers.</p>
+      </div>
+    </div>
+  );
+}
+
+export default Features;
