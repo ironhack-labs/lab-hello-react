c038d7c1ea337cf6c6c95d79a1df1379b1fe2bba
Chris Ziomek <cziomek1989@gmail.com>
2022-09-26 16:18:08 -0400

    done

---
+++ hello-react/src/images/ImageThree.js
@@ -0,0 +1,10 @@
+function ImageThree() {
+    return (
+      <img
+        src="https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/labs/landing-page/icon1.png"
+        alt="example"
+      />
+    );
+  }
+  
+  export default ImageThree;
---
+++ hello-react/src/images/ImageTwo.js
@@ -0,0 +1,10 @@
+function ImageTwo() {
+    return (
+      <img 
+        src="https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/labs/landing-page/menu-top-xs.png"
+        alt="example"
+      />
+    );
+  }
+  
+  export default ImageTwo;
