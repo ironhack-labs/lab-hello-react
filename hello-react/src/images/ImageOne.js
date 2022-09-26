4a5d883c9b266228d19c007e420317320f0777a8
Adam Szewczyk <aszewczyk1990@hotmail.com>
2022-09-26 13:39:18 -0400

    done

---
+++ hello-react/src/images/ImageOne.js
@@ -0,0 +1,12 @@
+
+
+const ImageOne = () => {
+    return (
+        <img
+        class="nav image"  
+        src="https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/labs/landing-page/ironhack-logo-xs.png" 
+        alt="Ironhack Logo"/>
+    )
+}
+
+export default ImageOne
---
+++ hello-react/src/images/ImageTwo.js
@@ -0,0 +1,11 @@
+const ImageTwo = () => {
+    return (
+        <img
+            class="nav image two"
+            src="https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/labs/landing-page/menu-top-xs.png"
+            alt="Nav Icon"
+        />
+    )
+}
+
+export default ImageTwo
