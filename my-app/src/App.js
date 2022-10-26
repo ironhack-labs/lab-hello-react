7678be9ad1d4f6574539ee634107c17f704386f1
Patrick-Groot <patrick@candis.io>
2022-10-26 15:07:48 +0200

    Second version

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -18,22 +18,22 @@ function App() {
         <button>Awesome!</button>
       </section>
       <section className="bottomSection">
-        <div>
+        <div className="block">
           <img src={icon1} alt="Declarative symbol"></img>
           <h2>Declarative</h2>
           <p>React makes it painless to create interactive UIs.</p>
         </div>
-        <div>
+        <div className="block">
           <img src={icon2} alt="Components symbol"></img>
           <h2>Components</h2>
           <p>Build encapsulated components that manage their state.</p>
         </div>
-        <div>
+        <div className="block">
           <img src={icon3} alt="Single-Way symbol"></img>
           <h2>Single-Way</h2>
           <p>A set of immutable values are passed to the components.</p>
         </div>
-        <div>
+        <div className="block">
           <img src={icon4} alt="JSX symbol"></img>
           <h2>JSX</h2>
           <p>Statically-typed, designed to run on modern browsers.</p>
