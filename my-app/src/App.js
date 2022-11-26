3c7d695f889277be36d9bed74c0819f47c98f6f2
Costache Robert Valentin <costache.robert.valentin@gmail.com>
2022-11-26 03:00:33 +0100

    Done

---
+++ my-app/src/App.js
@@ -0,0 +1,16 @@
+// Import App.css 
+import './App.css';
+
+// Import Componets 
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Footer/>
+    </div>
+  );
+}
+export default App;
---
+++ my-app/src/components/Header.js
@@ -0,0 +1,26 @@
+// Import images from  images folder
+import Logo from '../images/ironhack-logo-xs.png'
+import Menu from '../images/menu-top-xs.png';
+
+// Import Componetes 
+
+import Content from './Content-Header';
+
+
+function Header(){
+    return(
+        <header>
+            <section className="header-wrapper">
+                <nav>
+                    <img src={Logo} alt='Iron-hack Logo'></img>
+                    <img src={Menu} alt="Menu" ></img>
+                </nav>
+                <Content/>
+            </section>
+        </header>
+
+
+    );
+}
+
+export default Header;
