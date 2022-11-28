12a9425dd73abe048ff6227ff757bf6f85d429c4
Ezequiel Benitez <ezequielbv@gmail.com>
2022-11-28 17:32:18 +0100

    [BER OCT222 FT] Ezequiel Lab Done

--- lab-hello-react/src/App.js
+++ lab-hello-react/src/App.js
@@ -1,7 +1,19 @@
 import logo from './images/ironhack-logo-xs.png';
+import menu from './images/menu-top-xs.png';
+import img1 from './images/icon1.png';
+import img2 from './images/icon2.png';
+import img3 from './images/icon3.png';
+import img4 from './images/icon4.png';
 import './App.css';
 
 function App() {
+
+  const navigation = (
+    <nav>
+      <div><a href="/"><img src={logo}/></a></div>
+      <div><img src={menu}/></div>
+    </nav>
+  )
 
   const main = (
     <main>
@@ -15,22 +27,22 @@ function App() {
     <section className="features">
       <div className="cards">
         <div>
-          <img src="./images/icon1.png" alt="icon1"/>
+          <img src={img1} alt="icon1"/>
           <h3>Declarative</h3>
           <p>React makes it painless to create interactive UIs.</p>
         </div>
         <div>
-          <img src="./images/icon2.png" alt="icon2"/>
+          <img src={img2} alt="icon2"/>
           <h3>Components</h3>
           <p>Build encapsulated components that manage their state.</p>
         </div>
         <div>
-          <img src="./images/icon3.png" alt="icon3"/>
+          <img src={img3} alt="icon3"/>
           <h3>Single-Way</h3>
           <p>A set of immutable values are passed to the component's</p>
         </div>
         <div>
-          <img src="./images/icon4.png" alt="icon4"/>
+          <img src={img4} alt="icon4"/>
           <h3>JSX</h3>
           <p>Statically-typed, designed to run on modern browsers.</p>
         </div>
@@ -41,6 +53,7 @@ function App() {
 
   return (
     <div className="App">
+      {navigation}
       {main}
       {features}
     </div>
