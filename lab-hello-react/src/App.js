f9f837ea502a673acf9143640a67a4d5ad78d4f9
Arcadio REYES <arcadioreyesph@gmail.com>
2023-01-05 17:24:17 +0100

    iteration4 done

--- lab-hello-react/src/App.js
+++ lab-hello-react/src/App.js
@@ -1,8 +1,21 @@
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
+import NavigationBar from "./components/NavigationBar/NavigationBar.jsx";
+import MainSection from "./components/MainSection/MainSection.jsx";
+import SecondSection from "./components/SecondSection/SecondSection.jsx";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <NavigationBar />
+      <br />
+
+      <MainSection />
+      <br />
+
+      <SecondSection />
+    </div>
+  );
 }
 
 export default App;
