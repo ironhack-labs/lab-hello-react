850f4d9da9c2ad10750dcd85d7fb06789cd0ce7c
Dapato2 <108306801+Dapato2@users.noreply.github.com>
2023-01-25 19:00:19 -0600

    done

---
+++ src/content/Title/Title.jsx
@@ -0,0 +1,14 @@
+import "./title.css";
+
+const Title = () => {
+    return (
+        <div className="Title">
+            <h1><strong>Say hello to ReactJS</strong></h1>
+            <h4>
+            You will learn how to use the most popular frontend library, and become a super Ninja Developer
+            </h4>
+        </div>
+    )
+}
+
+export default Title;
