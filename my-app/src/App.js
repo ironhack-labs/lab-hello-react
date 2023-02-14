d5e6b5e80374fd67b3b7d69fe4f09863e608db81
tiagofaraujo <tiago_araujo@hotmail.com>
2023-02-14 20:40:51 +0000

    done int 1,2,3

---
+++ my-app/src/App.js
@@ -0,0 +1,26 @@
+// src/App.js
+import './App.css';
+// import the images
+import logo from './images/ironhack-logo-xs.png'
+import menu from './images/menu-top-xs (1).png'
+import icon1 from './images/icon1.png'
+import icon2 from './images/icon2.png'
+import icon3 from './images/icon4.png'
+import icon4 from './images/icon4.png'
+
+function App() {
+  return (
+    <div className="App">
+    <img src={logo} alt="logo" />
+    <img src={menu} alt="menu" />
+    <h1>Say hell to ReactJS</h1>
+    <h4>You will learn how to use the mos popular frontend library, and become a super Ninja developer.</h4>
+<button>Awesome!</button>
+
+
+
+
+    </div>
+  );
+}
+export default App;
