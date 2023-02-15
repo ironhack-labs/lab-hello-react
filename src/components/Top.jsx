3ed6b72acaff8af1399bf0a51e2ef72347f95a18
Zefanja Timorason <z.timorason@gmail.com>
2023-02-15 15:25:09 +0100

    only some styling left

---
+++ src/components/Top.jsx
@@ -0,0 +1,15 @@
+function Top() {
+    return (
+        <div className="top">
+            <h1 className="text">
+                Say hello to ReactJS
+            </h1>
+            <p className="text">You will learn how to use the most popular frontend library, and become a super ninja developer </p>
+            <img src='https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/labs/landing-page/ironhack-logo-xs.png' alt='ironhacklogo' className="logo"/>
+            <img src="https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/labs/landing-page/menu-top-xs.png" alt='menutop' className="menu"/>
+            <button className="awesome">Awesome!</button>
+        </div>
+    )
+}
+
+export default Top
