138a12854b3d442b9d0fe24da2b3901393a0dcc8
Jessica-Maria Freund <jessicamariafreund@gmail.com>
2023-02-16 18:37:07 +0100

    done.

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Header from './components/Header';
+import Main from './components/Main';
+
+function App() {
+  return (
+    <div className="App">
+      <Header></Header> 
+      <Main></Main>
+    </div>
+  );
+}
+export default App;
