c8a1ebaadf8935e1e61dc2cebad6c7996e31ceb3
Susi Mehl <susimehl@gmx.de>
2023-02-20 17:08:08 +0100

    "done"

---
+++ my/src/App.js
@@ -0,0 +1,41 @@
+
+import './App.css';
+
+
+function App() {
+  return (
+  <div className="App">
+    
+    <nav>
+                    <img id="Ironhack-logo" src="https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/labs/landing-page/ironhack-logo-xs.png" alt="ironhack-logo" />
+                    <img id="Lines" src="https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/labs/landing-page/menu-top-xs.png"  alt="lines" />
+                 
+                    <h1>Say hello to ReactJS</h1>
+                      <p>You will learn how to use <br/>
+                         the most popular frontend libary, <br/>
+                         and become a super Ninja developer.
+                      </p>
+          </nav>
+    
+
+              <a href="https://reactjs.org">
+                 <button className="btn">Awesome!</button>
+              </a>
+
+              <div>
+                   <img src="https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/labs/landing-page/icon1.png" alt="1"/>
+       
+                   <img src="https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/labs/landing-page/icon2.png" alt="2"/>
+                   
+                   <img src="https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/labs/landing-page/icon3.png" alt="3"/>
+                   
+                   <img src="https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/labs/landing-page/icon4.png" alt="4"/>
+              </div>
+
+
+
+  </div>
+  );
+}
+
+export default App;
