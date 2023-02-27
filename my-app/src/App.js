f05c67df3f5f825ec3a187e75eb79efdd155771a
Joao <joao621119@gmail.com>
2023-02-27 17:22:21 +0000

    done

---
+++ my-app/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import SingleComponent from './components/SingleComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <SingleComponent/>
+    </div>
+  );
+}
+
+export default App;
