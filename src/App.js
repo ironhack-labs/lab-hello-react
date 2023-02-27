42b9c9af58089c0becfbd7d4aad05958f3b5bad7
Cristina Chaparro <cricbau@gmail.com>
2023-02-27 18:04:07 +0100

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <div id="container">
+        <img
+          src="https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/labs/landing-page/ironhack-logo-xs.png"
+          alt="ironhack's logo"
+        />
+        <img
+          id="menu"
+          src="https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/labs/landing-page/menu-top-xs.png"
+          alt="hamburger menu logo"
+        />
+        <h1>
+          Say hello to
+          <br />
+          ReactJS
+        </h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          You will learn how to use
+          <br />
+          the most popular frontend library,
+          <br />
+          and become a super Ninja developer.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <a href="">Awesome!</a>
+      </div>
+      <div id="container-two">
+        <div className="card">
+          <img
+            src="https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/labs/landing-page/icon1.png"
+            alt="adjustable wrench icon"
+          />
+          <h3>Declarative</h3>
+          <p>React makes it painless to create interactive UIs.</p>
+        </div>
+        <div className="card">
+          <img
+            src="https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/labs/landing-page/icon2.png"
+            alt="video and pen icon"
+          />
+          <h3>Components</h3>
+          <p>Build encapsulated components that manage their state.</p>
+        </div>
+        <div className="card">
+          <img
+            src="https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/labs/landing-page/icon3.png"
+            alt="screw icon"
+          />
+          <h3>Single-Way</h3>
+          <p>A set of immutable values are passed to the component's.</p>
+        </div>
+        <div className="card">
+          <img
+            src="https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/labs/landing-page/icon4.png"
+            alt="html icon"
+          />
+          <h3>JSX</h3>
+          <p>Statically-typed, designed to run on modern browsers.</p>
+        </div>
+      </div>
     </div>
   );
 }
