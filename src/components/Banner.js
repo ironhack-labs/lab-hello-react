45231b5e6b8e9cb14e0d6ceed20d6d97e8167806
Joao Vitor <joao-vitor.fede@hpe.com>
2023-03-03 23:14:44 -0300

    done

---
+++ src/components/Banner.js
@@ -0,0 +1,15 @@
+
+
+function Banner (){
+    return (
+        <div className = "Banner"> 
+            <h1>Say Hello to ReactJS</h1>
+            <p>You will learn how to use the most popular frontend library, and become a super Ninja developer.</p>
+            <button>
+                Awesome!
+            </button>
+        </div>
+    )
+}
+
+export default Banner
---
+++ src/components/Components.js
@@ -0,0 +1,14 @@
+import icon2 from '../images/icon2.png';
+
+function Components (){
+    return(
+        <div className= "part2">
+            <img src ={icon2} alt = "icon1" />
+            <h3>Components</h3>
+            <p>Build encapsulated components that manage their state.</p>
+
+        </div>
+    )
+}
+
+export default Components
