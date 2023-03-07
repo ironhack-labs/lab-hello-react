8e9ff64f425bae0657a781dd769d6f901ea696ed
Natalia Nossack <nvcordeiro22@gmail.com>
2023-03-07 17:28:12 +0100

    solved lab

---
+++ src/Heading.js
@@ -0,0 +1,21 @@
+import IronHackLogo from "./Images/ironhack-logo-xs.png";
+import MenuBarImg from "./Images/menu-top-xs.png";
+
+const Logo = <img id="iron-logo" src={IronHackLogo} alt="IronHack Logo" />;
+const MenuBar = (
+  <a id="menu-bar" href="https://www.ironhack.com" target = "_blank" rel="noopener noreferrer">
+
+    <img src={MenuBarImg} alt="menu bar" />
+  </a>
+);
+const HeadingNav = "nav-bar";
+function Heading() {
+  return (
+    <nav className={HeadingNav}>
+      {Logo}
+      {MenuBar}
+    </nav>
+  );
+}
+
+export default Heading;
