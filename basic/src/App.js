62df659e0abd892ee810199c614636d81aad4414
YinongLi <yinong.li97@gmail.com>
2023-03-28 20:07:41 +0100

    solved lab

---
+++ basic/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import NavBar from './components/NavBar';
+import Title from './components/Title';
+import Items from './components/Items';
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+      <Title />
+      <Items />
+
+    </div>
+  );
+}
+
+export default App;
