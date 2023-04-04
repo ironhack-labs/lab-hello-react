a762fbedf51ad22528ef6e1164f8235212f55cf0
Michael <mickmhtong@gmail.com>
2023-04-05 00:44:52 +0800

    build: Iteration 4

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
-import './App.css';
+import "./App.css";
+import Button from "./components/Button";
+import Gallery from "./components/Gallery";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div className="App"></div>
+    <div className="App">
+      <Navbar />
+      <div
+        className="row dark"
+        style={{ height: "40vh", textAlign: "left", padding: "20px 0 0 80px" }}
+      >
+        <div className="col">
+          <h1>Say hello to ReactJS</h1>
+          <p>
+            You will learn how to use the most popular frontend library, and become a super Ninja
+            developer.
+          </p>
+        </div>
+        <div>
+          <Button mode="outline-" color="light" text="Awesome!" />
+        </div>
+      </div>
+      <div style={{ margin: "20px 0 0 0" }}>
+        <Gallery />
+      </div>
+    </div>
   );
 }
+
 export default App;
