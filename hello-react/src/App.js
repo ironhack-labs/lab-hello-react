842246931978e02121e23cce95538c3c3d33b91f
Rafal Kolibaba <rafal.kolibaba@icloud.com>
2023-05-01 15:47:38 +0200

    done

---
+++ hello-react/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css'
+import "./index.css"
+import TopSection from './components/TopSection'
+import BottomSection from './components/BottomSection'
+
+function App() {
+  return (
+    <div className="App">
+    <TopSection />
+    <BottomSection />
+    </div>
+  )
+}
+
+export default App
---
+++ hello-react/src/components/TopSection.js
@@ -0,0 +1,17 @@
+function TopSection() {
+    return (
+        <>
+        <nav>
+            <img src="../images/ironhack-logo-xs.png" alt="logo" />
+            <img src="../images/menu-top-xs.png" alt="menu" />
+        </nav>
+        <div className="sayHello">
+            <h1>Say hello to ReactJS</h1>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus labore eum voluptatibus placeat ut quia assumenda doloribus ad, aliquid deleniti, a maxime, cumque voluptas consequatur dolorum quas distinctio. At, libero!</p>
+            <button>AWESOME!</button>
+        </div>
+        </>
+    )
+}
+
+export default TopSection
