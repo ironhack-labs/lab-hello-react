6a4aa86c2416b0b5b41b04182fac99c6d39d130a
Diana Kalstein <dianakalstein@gmail.com>
2023-05-01 16:23:20 +0200

    Update Navbar.js

--- my-app/src/components/Navbar.js
+++ my-app/src/components/Navbar.js
@@ -1,29 +1,16 @@
 // 1. Create a function that returns HTML or JSX content
-import React from 'react';
-import ironhackLogo from '../images/ironhack-logo.png';
-import menuLogo from '../images/menu-logo.png';
+import ironhackLogo from "../images/ironhack-logo.png";
+import menuLogo from "../images/menu-logo.png";
 
-
-
-class Navbar extends React.Component() {
-    render() {
-    return (
-        <div>
+function Navbar() {
+  return (
+    <div>
       <nav>
-        <img
-          id="nav-img"
-          src={ironhackLogo}
-          alt="ironhackLogo"
-        />
-          <img
-          id="nav-img"
-          src={menuLogo}
-          alt="menuLogo"
-        />
+        <img id="nav-img" src={ironhackLogo} alt="ironhackLogo" />
+        <img id="nav-img" src={menuLogo} alt="menuLogo" />
       </nav>
-      </div>
-    );
-  }}
-  
-  export default Navbar;
-  
+    </div>
+  );
+}
+
+export default Navbar;
