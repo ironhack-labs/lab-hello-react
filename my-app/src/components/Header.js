5115425217339411e84f1b00bf80b166b523beb3
Shawn Wolter <woltersh@outlook.de>
2023-05-01 15:43:45 +0200

    finished lab

---
+++ my-app/src/components/Header.js
@@ -0,0 +1,13 @@
+function Header() {
+  return (
+    <header className="header">
+        <div className="container">
+            <h1>Say hello to ReactJs</h1>
+            <h2>You will learn how to use the most popular frontend library and become a super ninja developer.</h2>
+            <button>Awesome!</button>
+        </div>
+    </header>
+  );
+}
+
+export default Header;
