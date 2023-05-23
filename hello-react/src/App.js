6a2645c07be4a6a6d9f70bab43327fcddd1fab3b
RampriyaKarthick <118220230+RampriyaKarthick@users.noreply.github.com>
2023-05-23 21:30:42 +0200

    iteration 4-in progress

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -3,7 +3,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+      <nav className="navbar"> </nav>
+      <div className="container"><h1>Say Hello to </h1> <h1>Reactjs</h1> 
+      <h2>You will learn how to use <br>
+        The most popular front end library
+      </br> And become a super Ninja developer </h2>
+       </div>
     </div>
   );
 }
