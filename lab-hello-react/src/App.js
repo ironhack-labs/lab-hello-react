a829c9348598d6547d51cb90c0ead96797d5487a
jmaguayo95 <josemaguayo@icloud.com>
2023-05-24 18:55:29 +0200

    lab hecho

---
+++ lab-hello-react/src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Header from "./components/Header";
+import Body from "./components/Body";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Header />
+      <Body />
+    </div>
+  );
+}
+export default App;
---
+++ lab-hello-react/src/components/Navbar.js
@@ -0,0 +1,13 @@
+import ironhackLogo from "../images/ironhack-logo-xs.png";
+import dropdownMenuLogo from "../images/menu-top-xs.png";
+
+function Navbar() {
+  return (
+    <div className="Navbar d-flex">
+      <img src={ironhackLogo} class="App-logo"></img>
+      <img src={dropdownMenuLogo} class="dropdown-logo"></img>
+    </div>
+  );
+}
+
+export default Navbar;
