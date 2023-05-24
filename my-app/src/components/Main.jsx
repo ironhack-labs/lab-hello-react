8f9becf06cb591a45afba856d4f669117afd4f17
RonnySk <ronny_sk@hotmail.com>
2023-05-24 22:50:23 +0200

    Main finished.

--- my-app/src/components/Main.jsx
+++ my-app/src/components/Main.jsx
@@ -4,16 +4,20 @@ import menuImg from "../images/menu-top-xs.png";
 
 function Main() {
   return (
-    <div className="main">
+    <div className="Main">
       <div className="Navbar">
         <img className="Ironhack-Logo" src={ironhackLogo} alt="ironhack-logo" />
         <img className="Menu-Img" src={menuImg} alt="menu-logo" />
       </div>
       <section className="Section1">
-        <h1>Say hello to ReactJS</h1>
+        <h1>
+          Say hello to <br />
+          ReactJS
+        </h1>
         <p>
-          You will learn how to use the most popular frontend library, and
-          become a super Ninja developer.
+          You will learn how to use <br />
+          the most popular frontend library, <br />
+          and become a super Ninja developer.
         </p>
         <button>Awesome!</button>
       </section>
