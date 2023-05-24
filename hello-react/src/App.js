36cefd0fc00d98868c865e7e63ff5b0f269e6150
Lena Imdahl <mail@lenaimdahl.de>
2023-05-24 16:03:04 +0200

    styled text in App.js

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import "./App.css";
 import ironhack from "./images/ironhack.png";
 import topmenu from "./images/topmenu.png";
 import icon1 from "./images/icon1.png";
@@ -11,42 +11,49 @@ function App() {
     <div className="App">
       <nav className="navbar"> </nav>
       <div className="first-container">
-      <div className="img1"> <img src={ironhack} alt="" />
-      <img src={topmenu} alt="" />
-      </div> 
-      
-      <div className="text-line">
-        <h1>Say hello to</h1>
+        <div className="img1">
+          {" "}
+          <img src={ironhack} alt="" />
+          <img src={topmenu} alt="" />
+        </div>
+
+        <div className="text-line">
+          <h1>Say hello to</h1>
+          <h1>ReactJs</h1>
+        </div>
+
+        <div className="head">
+          <h4>
+            You will learn how to use
+            <h4>The most popular front end library,</h4>
+            And become a super Ninja developer.
+          </h4>
+        </div>
+
+        <button className="button">Awesome!</button>
       </div>
-      <div className="text-line">
-        <h1>ReactJs</h1>
-      </div> 
-
-      <div className="head">
-      <h4>You will learn how to use </h4>
-      <h4>The most popular front end library,</h4>
-      <h4>And become a super Ninja developer. </h4>
+      <div className="second-container">
+        <div className="text-container">
+          <img className="img-icon" src={icon1} alt="" />
+          <h2>Declarative</h2>
+          <h5>React makes it painless to create interactive UIs.</h5>
+        </div>
+        <div className="text-container">
+          <img className="img-icon" src={icon2} alt="" />
+          <h2>Components</h2>
+          <h5>React makes it painless to create interactive UIs.</h5>
+        </div>
+        <div className="text-container">
+          <img className="img-icon" src={icon3} alt="" />
+          <h2>Single-Way</h2>
+          <h5>React makes it painless to create interactive UIs.</h5>
+        </div>
+        <div className="text-container">
+          <img className="img-icon" src={icon4} alt="" />
+          <h2>JSX</h2>
+          <h5>React makes it painless to create interactive UIs.</h5>
+        </div>
       </div>
-
-       <button className="button">Awesome!</button>
-       </div>
-       <div className="second-container">
-<div><img src={icon1} alt="" />
-<h2>Declarative</h2>
-<h5>React makes it painless to create interactive UIs.</h5></div>
-<div><img src={icon2} alt="" />
-<h2>Components</h2>
-<h5>React makes it painless to create interactive UIs.</h5></div>
-<div><img src={icon3} alt="" />
-<h2>Single-Way</h2>
-<h5>React makes it painless to create interactive UIs.</h5></div>
-<div><img src={icon4} alt="" />
-<h2>JSX</h2><h5>React makes it</h5><h5>painless to create interactive UIs.</h5></div>
-
-
-
-
-       </div>
     </div>
   );
 }
