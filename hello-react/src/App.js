edb3617976c1118cd22922fbf5761cab1824f611
RampriyaKarthick <118220230+RampriyaKarthick@users.noreply.github.com>
2023-05-24 15:16:12 +0200

    top menu and ironhack logo

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,13 +1,33 @@
 import './App.css';
+import ironhack from "./images/ironhack.png";
+import topmenu from "./images/topmenu.png";
 
 function App() {
   return (
     <div className="App">
       <nav className="navbar"> </nav>
-      <div className="container"><h1>Say Hello to </h1> <h1>Reactjs</h1> 
-      <h2>You will learn how to use <br>
-        The most popular front end library
-      </br> And become a super Ninja developer </h2>
+      <div className="first-container">
+      <div className="img1"> <img src={ironhack} alt="" />
+      <img src={topmenu} alt="" />
+      </div> 
+      
+      <div className="text-line">
+        <h1>Say hello to</h1>
+      </div>
+      <div className="text-line">
+        <h1>ReactJs</h1>
+      </div> 
+
+      <div className="head">
+      <h2>You will learn how to use </h2>
+      <h2>The most popular front end library,</h2>
+      <h2>And become a super Ninja developer. </h2>
+      </div>
+
+       <button className="button">Awesome!</button>
+       </div>
+       <div className="second-container">
+
        </div>
     </div>
   );
