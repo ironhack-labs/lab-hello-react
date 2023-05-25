8f64da8140552cf0988540d6b51631473da2cb19
Matew1992 <mattia.dionori92@gmail.com>
2023-05-25 16:33:56 +0200

    80% complete

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,27 @@
 import logo from "./logo.svg";
 import "./App.css";
-import Header from "./component/Header";
+import Header from "./component/header.jsx";
+import Footer from "./component/footer.jsx";
 
 function App() {
-  const headline = "hello to react.js";
   return (
     <div className="App">
-      <Header headline = {headline}/>  
+    <div className="container-fluid" style={{ backgroundColor: "#1F2535" }}>
+    <div className="container">
+      <Header />
+      <div id="under-navbar" className="w-50 py-5">
+        <h1 className="text-white text-xl">Say Hello to ReactJS</h1>
+        <h3 className="text-white fs-4 my-3">
+          You will learn to use the most popular frontend library, and become a
+          super Ninja Developer
+        </h3>
+        <button class="btn btn-light fs-5 px-4 py-3 rounded-1 my-4">Awesome</button>
+      </div>
+      </div>
+    </div>
+    <div className="container py-5">
+    <Footer />
+    </div>
     </div>
   );
 }
--- my-app/src/component/Header.jsx
+++ my-app/src/component/Header.jsx
@@ -1,5 +1,15 @@
-function Header({headline}) {
-    return <header><h1> {headline} </h1></header>
-};
+import IronhackLogo from "../images/logo.png";
+import Menu from "../images/menu.png";
+import "../App.css";
+
+function Header() {
+  return (
+    <div className="header">
+      <h1>Say Hello to React.js</h1>
+      <img src={IronhackLogo} alt="Ironhack-logo" />
+      <img id="menu" src={Menu} alt="Menu-icon" />
+    </div>
+  );
+}
 
 export default Header;
