232b6cb8cd3d54a2f81d05cd37facd71c2a887aa
Quinta <13josueguardado@gmail.com>
2023-05-26 12:34:03 +0200

    lab unfinished

---
+++ my-app/src/components/Section.js
@@ -0,0 +1,17 @@
+function Section() {
+    return (
+        <div className="Section">
+            <h1>
+                Say Hello to
+            </h1>
+            <h1>ReactJS</h1>
+            <p className="paragraf">
+                You will learn how to use the most pupular frontend library, and become a super Ninja developer.
+            </p>
+            <button className="button">
+                Awesome!
+            </button>
+        </div>
+    );
+};
+export default Section;
