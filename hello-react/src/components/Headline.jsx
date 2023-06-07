ceac1ba27620653f127bbd833a5511d56383f7ff
rach <rachelpytse@gmail.com>
2023-06-08 00:27:48 +0200

    iteration 4 css

--- hello-react/src/components/Headline.jsx
+++ hello-react/src/components/Headline.jsx
@@ -2,16 +2,17 @@
 //
 // Your code here
 function Headline() {
-    return (
-        <div>
-            <h1>Say hello to ReactJS</h1>
-            <p>You will learn how to use the most popular frontend library, and become a super Ninja developer.</p>
-            <button>Awesome</button>
-        </div>
-    )
-        
-  }
-  // 2. Export the function:
-  // export default ComponentName
-  export default Headline;
-  
+  return (
+    <div id="headline">
+      <h1>Say hello to ReactJS</h1>
+      <p>
+        You will learn how to use the most popular frontend library, and become
+        a super Ninja developer.
+      </p>
+      <button>Awesome!</button>
+    </div>
+  );
+}
+// 2. Export the function:
+// export default ComponentName
+export default Headline;
--- hello-react/src/components/Intro.jsx
+++ hello-react/src/components/Intro.jsx
@@ -1,7 +1,7 @@
 import icon1 from "../assets/icon1.png";
 import icon2 from "../assets/icon2.png";
-import icon3 from '../assets/icon3.png'
-import icon4 from '../assets/icon4.png'
+import icon3 from "../assets/icon3.png";
+import icon4 from "../assets/icon4.png";
 
 // 1. Create a function that returns HTML or JSX content
 //
@@ -9,49 +9,29 @@ import icon4 from '../assets/icon4.png'
 
 function Intro() {
   return (
-    <div>
+    <div id="intro">
       <div className="container">
+        <img className="icon" src={icon1} alt="icon1" />
         <h2>Declarative</h2>
         <p>React make it painless to create interactive UIs.</p>
-        <img
-          className="icon"
-          src={icon1}
-          alt="icon1"
-        />
-        
       </div>
 
       <div className="container">
+        <img className="icon" src={icon2} alt="icon2" />
         <h2>Components</h2>
         <p>Build encapsulated components that manage their state</p>
-        <img
-          className="icon"
-          src={icon2}
-          alt="icon2"
-        />
-        
       </div>
 
       <div className="container">
+        <img className="icon" src={icon3} alt="icon3" />
         <h2>Single-Way</h2>
         <p>A set of immutable values are passed to the component's</p>
-        <img
-          className="icon"
-          src={icon3}
-          alt="icon3"
-        />
-        
       </div>
 
       <div className="container">
+        <img className="icon" src={icon4} alt="icon4" />
         <h2>JSX</h2>
         <p>Statically-typed, designed to run on modern browser</p>
-        <img
-          className="icon"
-          src={icon4}
-          alt="icon4"
-        />
-        
       </div>
     </div>
   );
