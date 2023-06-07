9a97be9991bfdf598beff9f651a27e3866c57a92
Rayane <alirayane14@gmail.com>
2023-06-07 19:13:27 +0200

    lab done sans style

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -3,7 +3,44 @@ import "./App.css";
 function App() {
   return (
     <>
-      <div className="App"></div>
+      <div className="App">
+        <header>
+          <img
+            src="https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/labs/landing-page/ironhack-logo-xs.png"
+            alt="logo IronHack"
+          />
+          <img src="https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/labs/landing-page/menu-top-xs.png" />
+        </header>
+
+        <div className="main">
+          <h1>Say hello to ReactJS</h1>
+
+          <p>You will learn how to use</p>
+          <p>the most popular frontend library,</p>
+          <p>and become a super Ninja developer.</p>
+
+          <button>Awesome!</button>
+        </div>
+
+        <div className="images">
+          <img
+            src="https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/labs/landing-page/icon1.png"
+            alt=""
+          />
+          <img
+            src="https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/labs/landing-page/icon2.png"
+            alt=""
+          />
+          <img
+            src="https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/labs/landing-page/icon3.png"
+            alt=""
+          />
+          <img
+            src="https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/labs/landing-page/icon4.png"
+            alt=""
+          />
+        </div>
+      </div>
     </>
   );
 }
