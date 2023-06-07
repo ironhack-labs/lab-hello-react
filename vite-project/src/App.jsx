b36c8e9660f5225854c5a14160af8fc7c646c1fc
Cedric Aim <cedric.aim@gmail.com>
2023-06-07 20:05:21 +0200

    ajout du design

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -4,41 +4,58 @@ function App() {
   return (
     <>
       <div className="App">
-        <header>
-          <img
-            src="https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/labs/landing-page/ironhack-logo-xs.png"
-            alt="logo IronHack"
-          />
-          <img src="https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/labs/landing-page/menu-top-xs.png" />
-        </header>
+        <section className="top">
+          <header>
+            <img
+              src="https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/labs/landing-page/ironhack-logo-xs.png"
+              alt="logo IronHack"
+            />
+            <img src="https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/labs/landing-page/menu-top-xs.png" />
+          </header>
 
-        <div className="main">
-          <h1>Say hello to ReactJS</h1>
+          <div className="main">
+            <h1>Say hello to ReactJS</h1>
 
-          <p>You will learn how to use</p>
-          <p>the most popular frontend library,</p>
-          <p>and become a super Ninja developer.</p>
+            <p>You will learn how to use</p>
+            <p>the most popular frontend library,</p>
+            <p>and become a super Ninja developer.</p>
 
-          <button>Awesome!</button>
-        </div>
-
+            <button>Awesome!</button>
+          </div>
+        </section>
         <div className="images">
-          <img
-            src="https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/labs/landing-page/icon1.png"
-            alt=""
-          />
-          <img
-            src="https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/labs/landing-page/icon2.png"
-            alt=""
-          />
-          <img
-            src="https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/labs/landing-page/icon3.png"
-            alt=""
-          />
-          <img
-            src="https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/labs/landing-page/icon4.png"
-            alt=""
-          />
+          <div className="image__content">
+            <img
+              src="https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/labs/landing-page/icon1.png"
+              alt=""
+            />
+            <h2>Declarative</h2>
+            <p>React makes it painless to create interactive UIs</p>
+          </div>
+          <div className="image__content">
+            <img
+              src="https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/labs/landing-page/icon2.png"
+              alt=""
+            />
+            <h2>Components</h2>
+            <p>Build encaspsulated components that manage their state.</p>
+          </div>
+          <div className="image__content">
+            <img
+              src="https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/labs/landing-page/icon3.png"
+              alt=""
+            />
+            <h2>Single-Way</h2>
+            <p>A set of immutable values are passed to the components</p>
+          </div>
+          <div className="image__content">
+            <img
+              src="https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/labs/landing-page/icon4.png"
+              alt=""
+            />
+            <h2>JSX</h2>
+            <p>Statically-typed designed to run on modern browsers</p>
+          </div>
         </div>
       </div>
     </>
