ac9083d66774b952fc7d1d6129ef459567ee8bff
robopecha <julikolb@gmail.com>
2023-07-10 16:30:54 +0200

    finished, no css

---
+++ reactlab/src/components/CardFour.jsx
@@ -0,0 +1,11 @@
+function CardFour() {
+  return (
+    <div>
+      <img src="src/assets/icon4.png" alt="icon4" />
+      <h3>JSX</h3>
+      <p>Statically-typed,<br/>designed to run on<br/>modern browsers.</p>
+    </div>
+  );
+}
+
+export default CardFour;
---
+++ reactlab/src/components/CardTwo.jsx
@@ -0,0 +1,11 @@
+function CardTwo() {
+  return (
+    <div>
+      <img src="src/assets/icon2.png" alt="icon2" />
+      <h3>Components</h3>
+      <p>Build encapsulated<br/>components that<br/>manage their state.</p>
+    </div>
+  );
+}
+
+export default CardTwo;
