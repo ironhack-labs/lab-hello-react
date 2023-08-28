9fe0cc6ddc37d06805cc46b1faf433d34cb7050d
Juan Cruz <j.cruzb@outlook.com>
2023-08-29 00:03:09 +0200

    The end

---
+++ lab-hello-react-vite/src/components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import './Header.css'
+import Awesome from '../Buttons/Awesome'
+
+function Header() {
+  return (
+    <header>
+      <h1>Say Hello to React Js</h1>
+      <p>You will learn how to use the most popular frontend library, and become a Super Ninja Developer🥷💻</p>
+        <Awesome />
+    </header>
+  );
+}
+
+export default Header;
