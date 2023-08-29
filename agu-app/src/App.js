df0dbde74263aaca73ed97592a846156219143cf
tngknzr <agustinparedes95@gmail.com>
2023-08-30 00:16:12 +0200

    first react

---
+++ agu-app/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+import TopPart from './components/TopPart';
+import Main from './components/Main';
+import Footer from './components/Footer';
+function App() {
+  return (
+    <div className="App">
+     
+      <TopPart/>
+      <Main/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
---
+++ agu-app/src/components/Main.js
@@ -0,0 +1,24 @@
+function Main() {
+    return(
+        <>
+        <div className="main-part">
+
+        <h1>Say hello to ReactJS</h1>
+        <p>You will learn how to use <br/>
+        the most popular frontend library,<br/>
+         and become a super ninja developer.</p>
+        <button>Awesome!</button>
+
+
+
+        </div>
+        
+        
+        
+        
+        
+        </>
+    )
+}
+
+export default Main
