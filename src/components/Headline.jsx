3719b30f51ae662c400fd5ed2de922c396d8dc33
You-Rim Kim <yourimykim@gmail.com>
2023-09-18 16:20:30 +0200

    lab done 2

---
+++ src/components/Headline.jsx
@@ -0,0 +1,18 @@
+function Headline() {
+    return (
+      <div>
+        <h1>Say hello to ReactJS</h1>
+        <p>You will learn how to use the most popular frontend library, and become a super Ninja developer.</p>
+       
+        <br/>
+
+        <a href="#">
+            <button className="button">Awesome!</button>
+        </a>
+
+      </div>
+    );
+  }
+  
+  export default Headline;
+  
---
+++ src/components/IconOne.jsx
@@ -0,0 +1,17 @@
+function IconOne() {
+    return (
+      <div>
+        <img
+            src="https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/labs/landing-page/icon1.png"
+            alt="IconTwo"
+        />
+
+        <h2>Declarative</h2>
+        <p>React makes it painless to create interactive UIs</p>
+
+      </div>
+    );
+  }
+  
+  export default IconOne;
+  
