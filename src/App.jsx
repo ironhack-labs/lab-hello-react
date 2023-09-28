7d7348f2866b3cf39031828b0c5d089e1b929a26
abrahamgw14 <agarciawals@gmail.com>
2023-09-28 13:43:17 +0200

    feat: lab done

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import './App.css';
+import Header from './header/Header';
+import HeaderText from './header/headertext/HeaderText'
+import Button from './button/Button'
+import Card from './Card/Card'
+
+
+function App() {
+  return (
+    <div className = "main">
+    <div className="header-main">
+       <Header />
+       <HeaderText />
+       <Button />
+    </div>
+    <div className = "card-group">
+      <Card 
+      image = "https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/labs/landing-page/icon1.png"
+      title = "Declarative"
+      p = "React make painless to create interactive UIs"
+      />
+      <Card 
+      image = "https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/labs/landing-page/icon2.png"
+      title = "Components"
+      p = "Built encapsulate components that manage their state"
+      />
+      <Card 
+      image = "https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/labs/landing-page/icon3.png"
+      title = "Single -way"
+      p = "A set of inmmutable values are passed to de component's "
+      />
+       <Card 
+      image = "https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/labs/landing-page/icon4.png"
+      title = "JSX"
+      p = "Statically-typed designed to run on modern browsers "
+      />
+    </div>
+    </div>
+  );
+}
+
+export default App;
