7af213fa89e7913068ee9b69520f4e0d735427b2
MaineKK <maralinew@gmail.com>
2023-10-15 17:59:27 +0200

    components

--- Hello-React/src/App.jsx
+++ Hello-React/src/App.jsx
@@ -1,43 +1,16 @@
 import './App.css';
+import Header from './components/Header';
+import FirstSection from './components/FirstSection';
+import Features from './components/Features';
 
 function App() {
   return (
     <div className="app">
-      <header>
-        <img src="./src/assets/ironhack-logo-xs.png" alt="Ironhack logo" />
-        <img className="mainSection" src="./src/assets/menu-top-xs.png" alt="main section" />
-      </header>
-      <section id="firstSection">
-        <h1>Say hello to ReactJS</h1>
-        <p>You will learn how to use</p>
-        <p>the most popular frontend library,</p>
-        <p>and become a super Ninja developer</p>
-        <button type="button" className="btn btn-light">Awesome!</button>
-      </section>
-      <section id="secondSection">
-        <article>
-          <img src="./src/assets/icon1.png" alt="Declarative" />
-          <h2>Declarative</h2>
-          <p>React makes it painless to create interactive UIs.</p>
-        </article>
-        <article>
-          <img src="./src/assets/icon2.png" alt="Components" />
-          <h2>Components</h2>
-          <p>Build encapsulated components that manage their state.</p>
-        </article>
-        <article>
-          <img src="./src/assets/icon3.png" alt="Single-Way" />
-          <h2>Single-Way</h2>
-          <p>A set of inmutable values are passed to the component's.</p>
-        </article>
-        <article>
-          <img src="./src/assets/icon4.png" alt="JSX" />
-          <h2>JSX</h2>
-          <p>Statically-typed, designed to run on modern browsers.</p>
-        </article>
-      </section>
+      <Header />
+      <FirstSection />
+      <Features />
     </div>
   );
 }
 
-export default App
+export default App;
