494f3bacbef822a0c272e4321c2a202b75e516e4
dutuka <39162585+dutuka@users.noreply.github.com>
2023-10-26 04:14:08 +0200

    done

---
+++ lab-hello-react-vite/src/components/Button.jsx
@@ -0,0 +1,9 @@
+function Button() {
+  return (
+    <a href="/">
+      <button className="btn">Awesome</button>
+    </a>
+  );
+}
+
+export default Button;
---
+++ lab-hello-react-vite/src/components/Gallery.jsx
@@ -0,0 +1,12 @@
+function Gallery() {
+  return (
+    <div id="gallery">
+      <img src="./src/assets/icon1.png" alt="icon1" />
+      <img src="./src/assets/icon2.png" alt="icon2" />
+      <img src="./src/assets/icon3.png" alt="icon3" />
+      <img src="./src/assets/icon4.png" alt="icon4" />
+    </div>
+  );
+}
+
+export default Gallery;
