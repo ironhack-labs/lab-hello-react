c885713ee3fbfa185479867cea496faeabadef27
João Cravidão <joao_kravidao@hotmail.com>
2023-10-31 16:02:06 +0000

    done

---
+++ my-first-app/src/Header.jsx
@@ -0,0 +1,9 @@
+export default function Header(){
+    return <>
+    <div className = "nav-bar">     
+            <img src="https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/labs/landing-page/ironhack-logo-xs.png" alt="logo" />
+            <img src="https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/labs/landing-page/menu-top-xs.png" alt="lines" />
+    </div>
+
+    </>
+}
