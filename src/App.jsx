887fa683958d52373b3798d11cafaacdc9bfb94f
Nadine Rangosch <nrangosch@gmail.com>
2024-01-18 12:50:19 +0100

    finished header, two sections and styling for each

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 
 import './App.css'
 import Header from './components/Header.jsx'
+import SectionOne from "./components/SectionOne.jsx"
+import SectionTwo from './components/SectionTwo.jsx'
 
 function App() {
 
   return (
     <div className="App">
     <Header></Header>
+    <SectionOne></SectionOne>
+    <SectionTwo></SectionTwo>
     </div>
   )
 }
---
+++ src/components/SectionOne.jsx
@@ -0,0 +1,20 @@
+import './SectionOne.css'
+
+function SectionOne(){
+    return(
+        <div id="section-one">
+        <div id="container" className='card'>
+        <h1 id="hello">
+            Say hello to ReactJS
+        </h1>
+        <p id="description">
+            You will learn how to use the most popular frontend library and become a super Ninja developer.
+        </p>
+        <button id="awesome"><a href="#">Awesome!</a></button>
+
+        </div>
+        </div>
+    )
+}
+
+export default SectionOne
