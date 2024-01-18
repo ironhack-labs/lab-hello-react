825821b06d8468ed68e6bf174213466c6bdf7f4e
Eleanor Pitcher <eleanorpitcher@gmail.com>
2024-01-18 10:58:02 +0100

    react components created, styling almost finished

---
+++ lab-hello-react-vite--/src/App.jsx
@@ -0,0 +1,16 @@
+// src/App.jsx
+import './App.css';
+import FirstComponent from './FirstComponent';
+import SecondComponent from './SecondComponent';
+
+
+function App() {
+  return (
+    <div className="App">
+      <FirstComponent></FirstComponent>
+      <SecondComponent></SecondComponent>
+    </div>
+  );
+}
+
+export default App;
---
+++ lab-hello-react-vite--/src/FirstComponent.jsx
@@ -0,0 +1,22 @@
+import './App.css'
+import logo from "./images/ironhack-logo-xs.png"
+import image1 from "./images/icon-1.png"
+
+
+function FirstComponent () {
+
+    return(
+        <div id="intro">
+            <div id="nav-bar">
+                <img className="logo" src={logo} alt="" />
+                <img id="menu-icon" src={image1} alt="" />
+            </div>
+
+            <h1>Say hello to ReactJS </h1>
+            <p id="text">You will learn how to use the most popular frontend library, and become a super Ninja developer</p>
+            <button>Awesome!</button>
+        </div>
+    )
+}
+
+export default FirstComponent
