3783c4c0a5f887a3acf4dacb77f935c900990d6b
Marta El Bay <martaelbaygiberti@gmail.com>
2024-02-10 17:59:27 +0100

    Done

---
+++ helloreact/src/Components.jsx/Element1.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Element() {
+  return (
+    <div>
+        <img src="../assets/lab1img2.png" alt="declarative" />
+        <h2>Declarative</h2>
+        <p>React makes it painless to create interactive UIs</p>
+    </div>
+  )
+}
+
+export default Element
---
+++ helloreact/src/Components.jsx/Element2.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Element2() {
+  return (
+    <div>
+        <img src="../assets/lab1img3.png" alt="components" />
+        <h2>Components</h2>
+        <p>Build encapsulated components that manage their state</p>
+    </div>
+  )
+}
+
+export default Element2
