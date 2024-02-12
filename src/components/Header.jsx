2ea5bd4216f80a7996f7732671ad92f434014fd5
Josep Climent <josepcliment92@gmail.com>
2024-02-12 18:35:34 +0100

    version final

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+function Header() {
+
+return (
+<header id="header">
+<h1>Say hello to<br />ReactJS</h1>
+<p>You will learn how to use<br />the most popular frontend library,<br />and become a super Ninja developer</p>
+<button>Awesome!</button>
+</header>
+
+)
+
+}
+export default Header
