ea2370f494e49fecfb77ce18edb43f242b5a7796
Abranucu <abrahamcastiellobarbas@gmail.com>
2024-02-12 16:06:34 +0100

    Faltan estilos

---
+++ react/src/App.jsx
@@ -0,0 +1,20 @@
+import Header from './Header';
+import Footer from './Footer';
+import Body from './Body';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+      <Header />
+
+      <Body />
+
+      <Footer />
+
+    </div>
+  );
+}
+
+export default App;
