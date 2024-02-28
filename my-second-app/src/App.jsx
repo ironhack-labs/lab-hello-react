7439320c6f68c1ae7ca44ef291901e3f52c24c91
Ama Williams <53152990+Tetsewa@users.noreply.github.com>
2024-02-29 00:23:27 +0100

    add iteration 4 components

--- my-second-app/src/App.jsx
+++ my-second-app/src/App.jsx
@@ -1,10 +1,25 @@
 // src/App.jsx
 import './App.css';
+import Awesome from './components/Awesome';
 import Images from './components/Images'
 
 function App() {
   return (
-    <div className="App"></div>
+    <div className="App">
+
+    <img src = "../assets/ironhack-logo-xs (1).png.png"/>
+
+    <img src = "../assets/menu-top-xs.png"/>
+
+    <h1>Say hello to ReactJS</h1>
+      <p>You will learn how to use the most popular frontend library, and become a super Ninja developer</p>
+
+      <Awesome/>
+
+      <Images/>
+
+
+    </div>
   );
 }
 
--- my-second-app/src/components/Images.jsx
+++ my-second-app/src/components/Images.jsx
@@ -2,11 +2,22 @@
 function Images() {
   return (
     <div>
+      
       <img src = "../assets/icon1.png"/>
-      <img src = "../assets/icon1.png"/>
+      <h3>Declarative</h3>
+      <p>React makes it painless to create interactive UIs.</p>
+      <img src = "../assets/icon2.png"/>
+      <h3>Components</h3>
+      <p>Build encapsulated components that manage their state.</p>
+      <img src = "../assets/icon3.png"/>
+      <h3>Single-Way</h3>
+      <p>A set of immutab;;e values are passed to the components.</p>
+      <img src = "../assets/icon4.png"/>
+      <h3>JSX</h3>
+      <p>Statically-typed, designed to run on modern browsers.</p>
 
     </div>
   )
 }
 
-export default Images
+export default Images;
